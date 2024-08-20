986f66216124aa5da11146a59bc796ab056825fd
strahinjaobradovicso <strahinjaobradovicso@gmail.com>
2024-08-20 17:53:25 +0200

    chart view

--- Controllers/TimeEntryController.cs
+++ Controllers/TimeEntryController.cs
@@ -1,4 +1,6 @@
+using ASPNET_MVC_ChartsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using WebApp.ViewModels;
 
 namespace WebApp.Controllers;
@@ -22,6 +24,20 @@ public class TimeEntryController : Controller {
     public async Task<IActionResult> Table(){
         var res = await GetTotalHours();
         return View(res.OrderByDescending(emp => emp.Total));
+    }
+
+    public async Task<IActionResult> Chart(){
+        var res = await GetTotalHours();
+        var sum = res.Sum(emp => emp.Total);
+
+        List<DataPoint> dataPoints = res
+        .Select(emp => new DataPoint(emp.Name, Math.Round((double)emp.Total / sum * 100)))
+        .OrderByDescending(x => x.Y)
+        .ToList();
+
+        ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+        ViewBag.Title = "Employee Total Time Worked";
+        return View();
     }
 
 }
